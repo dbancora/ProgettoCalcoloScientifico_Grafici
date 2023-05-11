46b6873a5486b8310b2d1612796212d47b08b22c
Davide Bancora <d.bancora@campus.unimib.it>
2023-05-11 10:18:13 +0200

    DAVIDE: - Migliorata visualizzazione grafici

--- Grafici.py
+++ Grafici.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Carica i dati dal file CSV
-data = pd.read_csv('dati_matlab.csv')
+data = pd.read_csv('dati_python.csv')
 data = data.sort_values(by='Size')
 
 # Combinazione delle colonne MatrixName e Size in una nuova lista di nomi delle matrici
@@ -12,27 +12,32 @@ matrix_names = [f"{name} ({size/(1024*1024):.2f} MB)" for name, size in zip(data
 # Crea il grafico a barre con doppio asse y
 fig, ax1 = plt.subplots()
 
-# Barre per la memoria
+# Barre per i tempi
 color = 'tab:red'
-ax1.set_ylabel('Memory (MB)', color=color)
-ax1.bar(matrix_names, data['MemoryDiff']/(1024*1024), color=color, width=-0.2, align='edge')
+ax1.set_ylabel('Time (s)', color=color)
+ax1.bar(matrix_names, data['Time'], color=color, width=-0.2, align='edge')
 ax1.tick_params(axis='y', labelcolor=color)
 
-for i, v in enumerate(data['MemoryDiff']):
-    ax1.annotate(f'{v/(1024*1024):.2f} MB', xy=(i-0.2, v/(1024*1024)), ha='center', va='bottom', color=color)
+for i, v in enumerate(data['Time']):
+    ax1.annotate(f'{v:.2f} s', xy=(i-0.2, v), ha='center', va='bottom', color=color)
 
 # Crea l'asse y per i tempi
 ax2 = ax1.twinx()
 
-# Barre per i tempi
+# Barre per la memoria
 color = 'tab:blue'
-ax2.set_ylabel('Time (s)', color=color)
-ax2.bar(matrix_names, data['Time'], color=color, width=0.2, align='edge', alpha=0.5)
+ax2.set_ylabel('Memory (MB)', color=color)
+ax2.bar(matrix_names, data['MemoryDiff']/(1024*1024), color=color, width=0.2, align='edge', alpha=0.5)
 ax2.tick_params(axis='y', labelcolor=color)
 
-# Aggiungi le etichette di testo per i tempi utilizzando il metodo annotate di Matplotlib
-for i, v in enumerate(data['Time']):
-    ax2.annotate(f'{v:.2f} s', xy=(i+0.2, v), ha='center', va='bottom', color=color)
+# Aggiungi le etichette di testo per la memoria utilizzando il metodo annotate di Matplotlib
+for i, v in enumerate(data['MemoryDiff']):
+    ax2.annotate(f'{v/(1024*1024):.2f} MB', xy=(i+0.2, v/(1024*1024)), ha='center', va='bottom', color=color)
+
+
+
+# Crea l'asse y per la memoria
+ax2 = ax1.twinx()
 
 # Aggiungi l'etichetta sull'asse x, inclinazione e titolo
 plt.xticks(rotation=45, ha='right')
@@ -60,7 +65,8 @@ plt.ylabel('Errore Relativo Normalizzato')
 
 # Aggiungi le etichette dell'errore relativo sopra ogni barra
 for i in range(len(matrix_names)):
-    plt.text(i, data['NormalizedError'][i]+0.05, f"{data['Error'][i]}", ha='center')
+    error_label = "{:.4e}".format(data['Error'][i])
+    plt.text(i, data['NormalizedError'][i]+0.05, error_label, ha='center')
 
 
 
